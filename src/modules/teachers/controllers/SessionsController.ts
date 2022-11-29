e94a9894e30f9015e4865a1ebc74d96cd2927efe
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2022-11-29 15:40:42 -0300

    Adicionando o controller para as sessões de professor e adicionando a criação de uma sessão nele

---
+++ src/modules/teachers/controllers/SessionsController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import CreateSessionsService from "../services/CreateSessionsService";
+
+export default class SessionsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+
+    const createSession = new CreateSessionsService();
+
+    const teacher = await createSession
+      .execute({
+        email,
+        password,
+      })
+      .catch((error) => {
+        response.statusCode = 401;
+        return error;
+      });
+
+    response.cookie("token", teacher.token, {
+      maxAge: new Date(Date.now() + 999999999).getTime(),
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+    });
+
+    return response.json(teacher.teacher);
+  }
+}
