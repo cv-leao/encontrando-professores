de360dcd5421d9f10184f89de97b2a7c2d74ce1c
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2022-11-28 23:27:29 -0300

    Criação do controller de professores e adição do CreateTeacherService no mesmo

---
+++ src/modules/teachers/controllers/TeachersController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import CreateTeacherService from "../services/CreateTeacherService";
+
+export default class TeachersController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password, samePasswords, matter } = request.body;
+
+    const createTeacher = new CreateTeacherService();
+
+    const teacher = createTeacher
+      .execute({
+        name,
+        email,
+        password,
+        samePasswords,
+        matter,
+      })
+      .catch((error) => {
+        response.statusCode = 400;
+        return error;
+      });
+
+    return response.json(teacher);
+  }
+}
