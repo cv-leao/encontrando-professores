148b7d1d3bec0b6237b622def4591d959a26dea0
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2022-11-29 14:25:07 -0300

    Criação do service para update de professor

---
+++ src/modules/teachers/services/UpdateTeacherService.ts
@@ -0,0 +1,44 @@
+import { Teachers } from "@prisma/client";
+import { prismaClient } from "../../../database/prismaClient";
+import AppError from "../../../shared/errors/AppError";
+
+interface IteacherToUpdate {
+  id: string;
+  name: string;
+}
+
+type ITeacherUpdated = Omit<Teachers, "password">;
+
+class UpdateTeacherService {
+  public async execute({
+    id,
+    name,
+  }: IteacherToUpdate): Promise<ITeacherUpdated> {
+    const teacherExists = await prismaClient.teachers.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!teacherExists) {
+      throw new AppError("Usuário não encontrado.");
+    }
+
+    const teacher = await prismaClient.teachers.update({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        matter: true,
+      },
+      data: {
+        name: name,
+      },
+      where: {
+        id: id,
+      },
+    });
+
+    return teacher;
+  }
+}
