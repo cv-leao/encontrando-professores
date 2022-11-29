0dc9499d3634f0f6799970dcc8d3d9102c2f25f3
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2022-11-29 17:27:54 -0300

    Criação do service ShowTeacher

---
+++ src/modules/teachers/services/ShowTeacherService.ts
@@ -0,0 +1,32 @@
+import { Teachers } from "@prisma/client";
+import { prismaClient } from "../../../database/prismaClient";
+import AppError from "../../../shared/errors/AppError";
+interface ITeacherToShow {
+  id: string;
+}
+
+type ShowTeacher = Omit<Teachers, "password">;
+
+class ShowUserService {
+  public async execute({ id }: ITeacherToShow): Promise<ShowTeacher> {
+    const teacher = await prismaClient.teachers.findUnique({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        matter: true,
+      },
+      where: {
+        id: id,
+      },
+    });
+
+    if (!teacher) {
+      throw new AppError("Usuário não encontrado.");
+    }
+
+    return teacher;
+  }
+}
+
+export default ShowUserService;
