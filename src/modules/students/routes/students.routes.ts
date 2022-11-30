054f29e730edc841ee51777c74c9c023c8ac16f1
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2022-11-29 21:30:37 -0300

    Criando a rota para BecomeAStudentOfATeacherService

--- src/modules/students/routes/students.routes.ts
+++ src/modules/students/routes/students.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import isAuthenticated from "../../../shared/http/middlewares/isAuthenticated";
 import StudentsController from "../controllers/StudentsController";
 
 const studentsRouter = Router();
 const studentsController = new StudentsController();
 
 studentsRouter.post("/create", studentsController.create);
+studentsRouter.post(
+  "/becomeAStudent",
+  isAuthenticated,
+  studentsController.becomeAStudentOfATeacher
+);
 
 export default studentsRouter;
